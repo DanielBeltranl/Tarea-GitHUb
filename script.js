581f345469392a4d1c2ea4e9758738c09da2cd85
POAPCA02LC0311 <SSDD@POAPCA02LC0311.duoc.acad>
2025-10-06 20:26:42 -0300

    error arreglado del id email en la funcion showUsers()

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showUsers() {
-  const email = document.getElementById("emmail").value;//error en id
+  const email = document.getElementById("email").value;//error corregido
   const age = document.getElementById("age").value; 
 
   console.log("Email:", email);
